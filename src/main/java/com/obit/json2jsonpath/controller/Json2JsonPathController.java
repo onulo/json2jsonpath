488c2f0d5decea18610cc96ef3c436c83d7f6807
Ondrej Barci <ondrej.barci@gmail.com>
2019-12-20 15:59:59 +0100

    remove thymeleaf and make rest controlelr

--- src/main/java/com/obit/json2jsonpath/controller/Json2JsonPathController.java
+++ src/main/java/com/obit/json2jsonpath/controller/Json2JsonPathController.java
@@ -4,16 +4,16 @@ import com.obit.json2jsonpath.service.JsonPathService;
 import java.util.List;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller(Json2JsonPathController.BASE_URL)
+@RestController
 public class Json2JsonPathController {
 
-    public static final String BASE_URL = "/";
+    private static final String JSON_2_JSON_PATH_BASE_URL = "/json2jsonPath";
+
     private final JsonPathService jsonPathService;
 
     @Autowired
@@ -21,21 +21,9 @@ public class Json2JsonPathController {
         this.jsonPathService = jsonPathService;
     }
 
-    @GetMapping
-    public String getMainPage() {
-        return "index";
-    }
-
-    @PostMapping
-    public String postJson(@RequestParam String json, Model model) {
-        try {
-            final JSONObject jsonObject = new JSONObject(json);
-            model.addAttribute("output", jsonPathService.generateJsonPaths(jsonObject));
-            model.addAttribute("json", jsonObject.toString(3));
-        } catch (Exception e) {
-            model.addAttribute("json", json);
-            model.addAttribute("error", e.getMessage());
-        }
-        return "index";
+    @CrossOrigin
+    @PostMapping(JSON_2_JSON_PATH_BASE_URL)
+    public List<String> generateJsonPaths(@RequestBody String json) {
+        return jsonPathService.generateJsonPaths(new JSONObject(json));
     }
 }
